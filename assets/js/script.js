fcdd90f20e2ad958b56f42d7d1151497c55fa456
Myrdin <myrdin@protonmail.com>
2019-04-27 18:14:18 +0200

    Remove hide class

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,13 +18,29 @@ function toggleDropdown() {
 
 //Fade in
 
+var el = document.getElementsByClassName('section')[0];
 
-function fadein() {
-       var el = document.getElementsByClassName('section')[0];
-       el.classList.remove('hide');
-       el.classList.add('show');
-}
-
-window.onload = fadein();
-
+function fadeIn(el) {
+       var opacity = 0;
+     
+       el.style.opacity = 0;
+       el.style.filter = '';
+     
+       var last = +new Date();
+       var tick = function() {
+         opacity += (new Date() - last) / 800;
+         el.style.opacity = opacity;
+         el.style.filter = 'alpha(opacity=' + (100 * opacity)|0 + ')';
+     
+         last = +new Date();
+     
+         if (opacity < 1) {
+           (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16);
+         }
+       };
+     
+       tick();
+     }
+     
+     fadeIn(el);
 
