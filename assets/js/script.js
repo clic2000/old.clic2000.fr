031cc4e2bf3151a7dcd84af94409f3aff106dd86
myrdin <myrdin@protonmail.com>
2019-06-18 20:30:53 +0200

    Updating javascript script for page loading

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,27 @@
 // Navigation timing
 
 window.addEventListener("load", function () {
-       let now = new Date().getTime();
-       let loadingTime = now - performance.timing.navigationStart;
+  let now = new Date().getTime();
+  let loadingTime = now - performance.timing.navigationStart;
 
-       document.querySelector(".output").innerText =
-              "Chargement de la page : " + loadingTime + " ms";
+  document.querySelector(".output").innerText =
+    "Chargement de la page : " + loadingTime + " ms";
+}, false);
+
+window.addEventListener("load", function () {
+  let now = new Date().getTime();
+  let loadingTime = now - performance.timing.navigationStart;
+
+  document.querySelector(".output-en").innerText =
+    "Page loading : " + loadingTime + " ms";
 }, false);
 
 // Menu Dropdown
 
 
 function toggleDropdown() {
-       var element = document.getElementById("menu");
-       element.classList.toggle("is-active");
+  var element = document.getElementById("menu");
+  element.classList.toggle("is-active");
 }
 
 //Fade in
@@ -21,26 +29,26 @@ function toggleDropdown() {
 var el = document.getElementsByClassName('section')[0];
 
 function fadeIn(el) {
-       var opacity = 0;
-     
-       el.style.opacity = 0;
-       el.style.filter = '';
-     
-       var last = +new Date();
-       var tick = function() {
-         opacity += (new Date() - last) / 800;
-         el.style.opacity = opacity;
-         el.style.filter = 'alpha(opacity=' + (100 * opacity)|0 + ')';
-     
-         last = +new Date();
-     
-         if (opacity < 1) {
-           (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16);
-         }
-       };
-     
-       tick();
-     }
-     
-     fadeIn(el);
+  var opacity = 0;
+
+  el.style.opacity = 0;
+  el.style.filter = '';
+
+  var last = +new Date();
+  var tick = function () {
+    opacity += (new Date() - last) / 800;
+    el.style.opacity = opacity;
+    el.style.filter = 'alpha(opacity=' + (100 * opacity) | 0 + ')';
+
+    last = +new Date();
+
+    if (opacity < 1) {
+      (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16);
+    }
+  };
+
+  tick();
+}
+
+fadeIn(el);
 
