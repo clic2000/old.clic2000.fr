d0a44df482d818775991557cc1ae23165546f885
myrdin <myrdin@protonmail.com>
2019-07-19 15:34:02 +0200

    Tracking in production

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,3 +48,17 @@ function fadeIn(el) {
 }
 
 fadeIn(el);
+
+// Fathom - simple website analytics - https://github.com/usefathom/fathom
+
+(function(f, a, t, h, o, m){
+  a[h]=a[h]||function(){
+  (a[h].q=a[h].q||[]).push(arguments)
+  };
+  o=f.createElement('script'),
+  m=f.getElementsByTagName('script')[0];
+  o.async=1; o.src=t; o.id='fathom-script';
+  m.parentNode.insertBefore(o,m)
+  })(document, window, '//stats.clic2000.fr/tracker.js', 'fathom');
+fathom('set', 'siteId', 'JEHMY');
+fathom('trackPageview');
