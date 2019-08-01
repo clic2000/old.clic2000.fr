590824c9f435b197515bbac971027824dae672b0
myrdin <myrdin@protonmail.com>
2019-08-01 21:32:42 +0200

    update script

--- assets/js/fathom.js
+++ assets/js/fathom.js
@@ -1,5 +1,4 @@
-<!-- Fathom - simple website analytics - https://github.com/usefathom/fathom -->
-<script>
+
 (function(f, a, t, h, o, m){
 	a[h]=a[h]||function(){
 		(a[h].q=a[h].q||[]).push(arguments)
@@ -11,5 +10,3 @@
 })(document, window, '//stats.clic2000.fr/tracker.js', 'fathom');
 fathom('set', 'siteId', 'JEHMY');
 fathom('trackPageview');
-</script>
-<!-- / Fathom -->
