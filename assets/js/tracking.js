a51bd7cb6344b9bbe5b26bce8831c004010db747
mathilde <41020854+mthld@users.noreply.github.com>
2020-07-28 09:59:18 +0200

    fix broken tracking link

--- assets/js/tracking.js
+++ assets/js/tracking.js
@@ -3,7 +3,7 @@
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
-    var u="//stats.clic2000.net/";
+    var u="//stats.clic2000.fr/";
     _paq.push(['setTrackerUrl', u+'matomo.php']);
     _paq.push(['setSiteId', '1']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
