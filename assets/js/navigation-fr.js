20c516c73788baa82f01b12867f6cca1a93baa00
mathilde <mathilde@clic2000.fr>
2020-09-29 14:57:00 +0200

    remove javascript output for loading times

--- assets/js/navigation-fr.js
+++
@@ -1,9 +0,0 @@
-// Navigation timing
-
-window.addEventListener("load", function () {
-    let now = new Date().getTime();
-    let loadingTime = now - performance.timing.navigationStart;
-  
-    document.querySelector(".output").innerText =
-      "Chargement de la page : " + loadingTime + " ms";
-  }, false);
