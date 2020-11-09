5ff543eb2afd47e7b2ba006b09a1ebe87c3d1748
mthld <mathilde@clic2000.fr>
2020-11-09 22:04:18 +0100

    readd mistakenly deleted Javascript navigation scripts

---
+++ assets/js/navigation-en.js
@@ -0,0 +1,8 @@
+//Navigation timing
+window.addEventListener("load", function () {
+    let now = new Date().getTime();
+    let loadingTime = now - performance.timing.navigationStart;
+  
+    document.querySelector(".output-en").innerText =
+      "Page loading : " + loadingTime + " ms";
+  }, false);
