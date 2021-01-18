f41491413d3627d3ab332a232e18de26ea04da19
mthld <mathilde@clic2000.fr>
2021-01-18 11:48:45 +0100

    create chatbox.js and add link to all pages

---
+++ assets/js/chatbox.js
@@ -0,0 +1,1 @@
+window.$crisp=[];window.CRISP_WEBSITE_ID="7c161911-66bf-4541-b2ec-9d25b482017c";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
